e5cf33ebd0099a445ac4168f65ecf0a4cbc69be2
Ankit Goyal <ankitgo@users.noreply.github.com>
2015-10-09 17:37:18 +0530

    fix file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	print "<h2>php program to Add two numbers...</h2><br />";
               $val1 = 10;
               $val2 = 20;
-              $sum = $val2 + $val2;   /* Assignment operator */
+              $sum = $val1 + $val2;   /* Assignment operator */
               echo "<h1>Result(SUM): $sum</h1>";
 ?>
 
